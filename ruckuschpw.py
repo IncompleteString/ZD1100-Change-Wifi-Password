660e5001b5fd2f17044ae3d7f5402db60c576b6e
Chris Brown <17124269+IncompleteString@users.noreply.github.com>
2019-11-21 03:18:10 -1100

    Update ruckuschpw.py

--- ruckuschpw.py
+++ ruckuschpw.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python 
+#
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# <chris@pagotechinc.com> wrote this file.  As long as you retain this notice you
+# can do whatever you want with this stuff. If you think this stuff is worth is 
+# worth it, please buy me a beer in return.   Chris Brown
+# ----------------------------------------------------------------------------
+#
 import sys
 import pexpect
 import string
@@ -8,12 +16,14 @@ import smtplib
 import ssl
 
 #chooses a random 8 digit number to use as the wifi password
-n = 8
-PW=''.join(["{}".format(randint(0, 9)) for num in range(0, n)])
+print("creating a random 8 digit wifi password")
+n = 8 #selects number of digits in random number
+PW=''.join(["{}".format(randint(0, 9)) for num in range(0, n)]) #generates a random number of "N" digits
 
-PWSTR=("open wpa2 passphrase " + str(PW) + " algorithm AES")
+PWSTR=("open wpa2 passphrase " + str(PW) + " algorithm AES") # command to be passed to the zonedirector
 
-#logs into the Zonedirector and changes the password of the SSID to the random password
+#logs into the Zonedirector and changes the password of the SSID to the random number generated above
+print("logging into the zonedirector")
 child = pexpect.spawn ("ssh <IP address of target>")
 child.expect ('Please login:') 
 child.sendline ('<Username>') 
@@ -33,18 +43,19 @@ child.expect('#')
 child.sendline('exit')
 child.expect ('ruckus#')
 child.sendline('exit')
-#print (child.before) #this object refers to the output of the last child call 
-#child.interact() #puts me on the AP CLI 
+print (child.before)
+#child.interact() 
 child.expect(pexpect.EOF, timeout=20)
-sys.exit()
+
 
 
 #Emails the new password to the specified emails
-port = 465  # For SSL
-password = str("<Email Password>")
-sender_email = "<Email Account""
-receiver_email = "<recipent1>","<recipent2>","<recipent3>
-message = ("Subject: Guest Wifi Password"+"\n"+"\n"+"\n"+ "The new password for the Guest Wifi netowrk is: "+ str(PW))
+print("Emailing password to specified emails")
+port = 465  #SSL Port Number
+sender_email = "<Email Account>" #email account that will be sending the messages
+password = str("<Email Password>") #Password of the email address sending the messages
+receiver_email = "<recipent1>","<recipent2>","<recipent3>" #email address that will recieve the message
+message = ("Subject: Guest Wifi Password"+"\n"+"\n"+"\n"+ "The new password for the Guest Wifi netowrk is: "+ str(PW)+"\n"+"\n"+"\n"+"This Message was automatically genernated with Python, please see https://github.com/IncompleteString/ZD1100-Change-Wifi-Password/edit/master/ruckuschpw.py for source code")
 
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
@@ -52,4 +63,5 @@ with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     server.sendmail(sender_email, receiver_email, message)
 
 
+print("Success!")
 sys.exit()
