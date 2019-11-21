17720b5359c79ee77d17f06e798b9a9a3af21d40
Chris Brown <17124269+IncompleteString@users.noreply.github.com>
2019-11-21 03:58:56 -1100

    Update ruckuschpw.py

--- ruckuschpw.py
+++ ruckuschpw.py
@@ -21,7 +21,7 @@ print("creating a random 8 digit wifi password")
 n = 8 #selects number of digits in random number
 PW=''.join(["{}".format(randint(0, 9)) for num in range(0, n)]) #generates a random number of "N" digits
 
-PWSTR=("open wpa2 passphrase " + str(PW) + " algorithm AES") # command to be passed to the zonedirector
+
 
 
 #Varibles for use in logging into and controlling the zonedirector
@@ -29,7 +29,7 @@ Target=str("<Target Device IP>")
 username=str("<username of zonedirector")
 password=str("<password of zonedirector>")
 WLAN=str('"<Name of SSID>"')
-Command=("open wpa2 passphrase " + str(PW) + " algorithm AES")
+Command=("open wpa2 passphrase " + str(PW) + " algorithm AES") # command to be passed to the zonedirector
 
 #logs into the Zonedirector and changes the password of the SSID to the random number generated above
 print("logging into the zonedirector")
@@ -58,12 +58,14 @@ child.expect(pexpect.EOF, timeout=20)
 
 
 
+
+
 #Emails the new password to the specified email addresses
 print("Emailing password to specified emails")
 port = 465  #SSL Port Number
 sender_email = "<Email Account>" #email account that will be sending the messages
 password = str("<Email Password>") #Password of the email address sending the messages
-receiver_email = "<recipent1>","<recipent2>","<recipent3>" #email address that will recieve the message
+receiver_email = "<recipent1>","<recipent2>","<recipent3>" #email addresses that will recieve the message
 message = ("Subject: Guest Wifi Password"+"\n"+"\n"+"\n"+ "The new password for the Guest Wifi netowrk is: "+ str(PW)+"\n"+"\n"+"\n"+"This Message was automatically genernated with Python." +"\n"+"\n"+"\n""Please see https://github.com/IncompleteString/ZD1100-Change-Wifi-Password/edit/master/ruckuschpw.py for source code")
 
 context = ssl.create_default_context()
